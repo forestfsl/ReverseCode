a34c80c43b84d8d84963acdd8ed706941c3a2d68
fengsl <fengsl@lezhong.game>
2020-05-04 11:22:16 +0800

    删除多余文件

--- 001--fishHookDemo/001--fishHookDemo/ViewController.m
+++ 001--fishHookDemo/001--fishHookDemo/ViewController.m
@@ -8,32 +8,105 @@
 
 #import "ViewController.h"
 #import "fishhook.h"
+#import "SSZipArchive.h"
 
 @interface ViewController ()
+@property (copy, nonatomic) NSString *samplePath;
+@property (copy, nonatomic) NSString *zipPath;
+
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Private
+- (NSString *)tempZipPath {
+    NSString *path = [NSString stringWithFormat:@"%@/%@.zip",
+                      NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0],
+                      [NSUUID UUID].UUIDString];
+    return path;
+}
 
+- (NSString *)tempUnzipPath {
+    NSString *path = [NSString stringWithFormat:@"%@/%@",
+                      NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0],
+                      [NSUUID UUID].UUIDString];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSError *error = nil;
+    [[NSFileManager defaultManager] createDirectoryAtURL:url
+                             withIntermediateDirectories:YES
+                                              attributes:nil
+                                                   error:&error];
+    if (error) {
+        return nil;
+    }
+    return url.path;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     
+//
+//    //rebinding结构体
+//    struct rebinding nslog;
+//    nslog.name = "NSLog";
+//    nslog.replacement = myNslog;
+//    nslog.replaced = (void *)&sys_nslog;
+//    //rebinding结构体数组
+//    struct rebinding rebs[1] = {nslog};
+//    /**
+//     * 存放rebinding结构体的数组
+//     * 数组的长度
+//     */
+//    rebind_symbols(rebs, 1);
     
-    //rebinding结构体
-    struct rebinding nslog;
-    nslog.name = "NSLog";
-    nslog.replacement = myNslog;
-    nslog.replaced = (void *)&sys_nslog;
-    //rebinding结构体数组
-    struct rebinding rebs[1] = {nslog};
-    /**
-     * 存放rebinding结构体的数组
-     * 数组的长度
-     */
-    rebind_symbols(rebs, 1);
+    _samplePath = [[NSBundle mainBundle].bundleURL
+                      URLByAppendingPathComponent:@"Sample Data"
+                      isDirectory:YES].path;
+        NSLog(@"Sample path: %@", _samplePath);
+        
+       
+        _zipPath = [self tempZipPath];
+           NSLog(@"Zip path: %@", _zipPath);
+       
+           BOOL success = [SSZipArchive createZipFileAtPath:_zipPath
+                                    withContentsOfDirectory:_samplePath
+                                        keepParentDirectory:NO
+                                           compressionLevel:-1
+                                                   password: nil
+                                                        AES:YES
+                                            progressHandler:nil];
+           if (success) {
+               NSLog(@"Success zip");
+    
+           } else {
+               NSLog(@"No success zip");
+               
+           }
+       
+    }
+
+    #pragma mark - IBAction
+    - (IBAction)zipPressed:(id)sender {
+        _zipPath = [self tempZipPath];
+        NSLog(@"Zip path: %@", _zipPath);
+  
+        BOOL success = [SSZipArchive createZipFileAtPath:_zipPath
+                                 withContentsOfDirectory:_samplePath
+                                     keepParentDirectory:NO
+                                        compressionLevel:-1
+                                                password:nil
+                                                     AES:YES
+                                         progressHandler:nil];
+        if (success) {
+            NSLog(@"Success zip");
+
+        } else {
+            NSLog(@"No success zip");
+     
+        }
+
 }
 //---------------------------------更改NSLog-----------
 //函数指针
